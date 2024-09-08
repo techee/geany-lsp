f45cf168957da6a558379bb71e00caaab7df8f0d
Jiří Techet <techet@gmail.com>
2024-09-08 20:19:22 +0200

    Split handle_call() into multiple functions

--- lsp/src/lsp-rpc.c
+++ lsp/src/lsp-rpc.c
@@ -131,10 +131,207 @@ static void reply_async(LspServer *srv, const gchar *method, JsonrpcClient *clie
 }
 
 
+static GVariant *create_progress(LspServer *srv, GVariant *params)
+{
+	gboolean have_token = FALSE;
+	const gchar *token_str = NULL;
+	gint64 token_int = 0;
+
+	have_token = JSONRPC_MESSAGE_PARSE(params,
+		"token", JSONRPC_MESSAGE_GET_STRING(&token_str)
+	);
+	if (!have_token)
+	{
+		have_token = JSONRPC_MESSAGE_PARSE(params,
+			"token", JSONRPC_MESSAGE_GET_INT64(&token_int)
+		);
+	}
+
+	if (srv && have_token)
+	{
+		LspProgressToken token = {token_int, (gchar *)token_str};
+		lsp_progress_create(srv, token);
+	}
+
+	return NULL;
+}
+
+
+static GVariant *apply_edit(LspServer *srv, GVariant *params)
+{
+	GVariant *edit, *msg;
+	gboolean success;
+
+	success = JSONRPC_MESSAGE_PARSE(params,
+		"edit", JSONRPC_MESSAGE_GET_VARIANT(&edit)
+	);
+
+	if (success)
+		success = lsp_utils_apply_workspace_edit(edit);
+
+	msg = JSONRPC_MESSAGE_NEW(
+		"applied", JSONRPC_MESSAGE_PUT_BOOLEAN(success)
+	);
+
+	if (edit)
+		g_variant_unref(edit);
+
+	return msg;
+}
+
+
+static GVariant *workspace_configuration(LspServer *srv, GVariant *params)
+{
+	GVariantIter *iter = NULL;
+	GVariant *res = NULL;
+
+	JSONRPC_MESSAGE_PARSE(params,
+		"items", JSONRPC_MESSAGE_GET_ITER(&iter)
+	);
+
+	if (iter)
+	{
+		JsonNode *settings = lsp_utils_parse_json_file(srv->config.initialization_options_file,
+			srv->config.initialization_options);
+		JsonNode *empty_elem = json_from_string("{}", NULL);
+		JsonBuilder *builder = json_builder_new();
+		GVariant *member = NULL;
+		JsonNode *res_json = NULL;
+
+		json_builder_begin_array(builder);
+
+		while (g_variant_iter_loop(iter, "v", &member))
+		{
+			const gchar *section = NULL;
+			gboolean added = FALSE;
+
+			if (JSONRPC_MESSAGE_PARSE(member, "section", JSONRPC_MESSAGE_GET_STRING(&section)))
+			{
+				gchar *path = g_strconcat("$.", section, NULL);
+				JsonNode *matched = json_path_query(path, settings, NULL);
+
+				if (matched)
+				{
+					JsonArray *arr = json_node_get_array(matched);
+					if (arr && json_array_get_length(arr) > 0)
+					{
+						json_builder_add_value(builder, json_array_dup_element(arr, 0));
+						added = TRUE;
+					}
+					json_node_unref(matched);
+				}
+
+				g_free(path);
+			}
+
+			if (!added)
+				json_builder_add_value(builder, json_node_ref(empty_elem));
+		}
+
+		json_builder_end_array(builder);
+		res_json = json_builder_get_root(builder);
+		res = g_variant_take_ref(json_gvariant_deserialize(res_json, NULL, NULL));
+
+		g_variant_iter_free(iter);
+		json_node_free(settings);
+		json_node_free(res_json);
+		g_object_unref(builder);
+		json_node_unref(empty_elem);
+	}
+
+	return res;
+}
+
+
+static GVariant *show_document(LspServer *srv, GVariant *params)
+{
+	const gchar *uri = NULL;
+	gboolean external = FALSE;
+	gboolean success = FALSE;
+	GVariant *msg;
+
+	JSONRPC_MESSAGE_PARSE(params,
+		"uri", JSONRPC_MESSAGE_GET_STRING(&uri)
+	);
+
+	JSONRPC_MESSAGE_PARSE(params,
+		"external", JSONRPC_MESSAGE_GET_BOOLEAN(&external)
+	);
+
+	if (uri)
+	{
+		if (external || !g_str_has_prefix(uri, "file://"))
+		{
+			utils_open_browser(uri);
+			success = TRUE;
+		}
+		else if (g_str_has_prefix(uri, "file://"))
+		{
+			gchar *fname = lsp_utils_get_real_path_from_uri_locale(uri);
+			document_open_file(fname, FALSE, NULL, NULL);
+			g_free(fname);
+			success = TRUE;
+		}
+	}
+
+	msg = JSONRPC_MESSAGE_NEW(
+		"success", JSONRPC_MESSAGE_PUT_BOOLEAN(success)
+	);
+
+	return msg;
+}
+
+
+static GVariant *workspace_folders(LspServer *srv, GVariant *params)
+{
+	GPtrArray *folders = lsp_workspace_folders_get();
+	GVariant *msg = NULL;
+	guint num = 0;
+
+	foreach_document(num)
+	{
+		if (num > 1)
+			break;
+	}
+
+	if (num > 1)  // non-single-open document variant
+	{
+		GPtrArray *arr = g_ptr_array_new_full(1, (GDestroyNotify) g_variant_unref);
+		gchar *folder;
+		guint i;
+
+		foreach_ptr_array(folder, i, folders)
+		{
+			gchar *uri = g_filename_to_uri(folder, NULL, NULL);
+			GVariant *folder_variant;
+
+			folder_variant = JSONRPC_MESSAGE_NEW(
+				"uri", JSONRPC_MESSAGE_PUT_STRING(uri),
+				"name", JSONRPC_MESSAGE_PUT_STRING(folder)
+			);
+			g_ptr_array_add(arr, folder_variant);
+
+			g_free(uri);
+		}
+
+		msg = g_variant_take_ref(g_variant_new_array(G_VARIANT_TYPE_VARDICT,
+			(GVariant **)arr->pdata, arr->len));
+
+		g_ptr_array_free(arr, TRUE);
+	}
+
+	g_ptr_array_free(folders, TRUE);
+
+	return msg;
+}
+
+
 static gboolean handle_call(JsonrpcClient *client, gchar* method, GVariant *id, GVariant *params,
 	gpointer user_data)
 {
 	LspServer *srv = g_hash_table_lookup(client_table, client);
+	gboolean handled = FALSE;
+	GVariant *msg = NULL;
 
 	if (!srv)
 		return FALSE;
@@ -146,205 +343,38 @@ static gboolean handle_call(JsonrpcClient *client, gchar* method, GVariant *id, 
 
 	if (g_strcmp0(method, "window/workDoneProgress/create") == 0)
 	{
-		gboolean have_token = FALSE;
-		const gchar *token_str = NULL;
-		gint64 token_int = 0;
-
-		have_token = JSONRPC_MESSAGE_PARSE(params,
-			"token", JSONRPC_MESSAGE_GET_STRING(&token_str)
-		);
-		if (!have_token)
-		{
-			have_token = JSONRPC_MESSAGE_PARSE(params,
-				"token", JSONRPC_MESSAGE_GET_INT64(&token_int)
-			);
-		}
-
-		if (srv && have_token)
-		{
-			LspProgressToken token = {token_int, (gchar *)token_str};
-			lsp_progress_create(srv, token);
-		}
-
-		reply_async(srv, method, client, id, NULL);
-		return TRUE;
+		msg = create_progress(srv, params);
+		handled = TRUE;
 	}
 	else if (g_strcmp0(method, "workspace/applyEdit") == 0)
 	{
-		GVariant *edit, *msg;
-		gboolean success;
-
-		success = JSONRPC_MESSAGE_PARSE(params,
-			"edit", JSONRPC_MESSAGE_GET_VARIANT(&edit)
-		);
-
-		if (success)
-			success = lsp_utils_apply_workspace_edit(edit);
-
-		msg = JSONRPC_MESSAGE_NEW(
-			"applied", JSONRPC_MESSAGE_PUT_BOOLEAN(success)
-		);
-
-		reply_async(srv, method, client, id, msg);
-
-		g_variant_unref(msg);
-		if (edit)
-			g_variant_unref(edit);
-		return TRUE;
+		msg = apply_edit(srv, params);
+		handled = TRUE;
 	}
 	// we officially don't support this (not advertised in initialize's
 	// workspace/configuration) but some servers ask for it anyway so do our
 	// best in this case
 	else if (g_strcmp0(method, "workspace/configuration") == 0)
 	{
-		GVariantIter *iter = NULL;
-		GVariant *res = NULL;
-
-		JSONRPC_MESSAGE_PARSE(params,
-			"items", JSONRPC_MESSAGE_GET_ITER(&iter)
-		);
-
-		if (iter)
-		{
-			JsonNode *settings = lsp_utils_parse_json_file(srv->config.initialization_options_file,
-				srv->config.initialization_options);
-			JsonNode *empty_elem = json_from_string("{}", NULL);
-			JsonBuilder *builder = json_builder_new();
-			GVariant *member = NULL;
-			JsonNode *res_json = NULL;
-
-			json_builder_begin_array(builder);
-
-			while (g_variant_iter_loop(iter, "v", &member))
-			{
-				const gchar *section = NULL;
-				gboolean added = FALSE;
-
-				if (JSONRPC_MESSAGE_PARSE(member, "section", JSONRPC_MESSAGE_GET_STRING(&section)))
-				{
-					gchar *path = g_strconcat("$.", section, NULL);
-					JsonNode *matched = json_path_query(path, settings, NULL);
-
-					if (matched)
-					{
-						JsonArray *arr = json_node_get_array(matched);
-						if (arr && json_array_get_length(arr) > 0)
-						{
-							json_builder_add_value(builder, json_array_dup_element(arr, 0));
-							added = TRUE;
-						}
-						json_node_unref(matched);
-					}
-
-					g_free(path);
-				}
-
-				if (!added)
-					json_builder_add_value(builder, json_node_ref(empty_elem));
-			}
-
-			json_builder_end_array(builder);
-			res_json = json_builder_get_root(builder);
-			res = g_variant_take_ref(json_gvariant_deserialize(res_json, NULL, NULL));
-
-			g_variant_iter_free(iter);
-			json_node_free(settings);
-			json_node_free(res_json);
-			g_object_unref(builder);
-			json_node_unref(empty_elem);
-		}
-
-		reply_async(srv, method, client, id, res);
-
-		if (res)
-			g_variant_unref(res);
-		return TRUE;
+		msg = workspace_configuration(srv, params);
+		handled = TRUE;
 	}
 	else if (g_strcmp0(method, "window/showDocument") == 0)
 	{
-		const gchar *uri = NULL;
-		gboolean external = FALSE;
-		gboolean success = FALSE;
-		GVariant *msg;
-
-		JSONRPC_MESSAGE_PARSE(params,
-			"uri", JSONRPC_MESSAGE_GET_STRING(&uri)
-		);
-
-		JSONRPC_MESSAGE_PARSE(params,
-			"external", JSONRPC_MESSAGE_GET_BOOLEAN(&external)
-		);
-
-		if (uri)
-		{
-			if (external || !g_str_has_prefix(uri, "file://"))
-			{
-				utils_open_browser(uri);
-				success = TRUE;
-			}
-			else if (g_str_has_prefix(uri, "file://"))
-			{
-				gchar *fname = lsp_utils_get_real_path_from_uri_locale(uri);
-				document_open_file(fname, FALSE, NULL, NULL);
-				g_free(fname);
-				success = TRUE;
-			}
-		}
-
-		msg = JSONRPC_MESSAGE_NEW(
-			"success", JSONRPC_MESSAGE_PUT_BOOLEAN(success)
-		);
-
-		reply_async(srv, method, client, id, msg);
-
-		g_variant_unref(msg);
-		return TRUE;
+		msg = show_document(srv, params);
+		handled = TRUE;
 	}
 	else if (g_strcmp0(method, "workspace/workspaceFolders") == 0)
 	{
-		GPtrArray *folders = lsp_workspace_folders_get();
-		guint num = 0;
+		msg = workspace_folders(srv, params);
+		handled = TRUE;
+	}
 
-		foreach_document(num)
-		{
-			if (num > 1)
-				break;
-		}
-
-		if (num > 1)  // non-single-open document variant
-		{
-			GPtrArray *arr = g_ptr_array_new_full(1, (GDestroyNotify) g_variant_unref);
-			GVariant *folders_variant;
-			gchar *folder;
-			guint i;
-
-			foreach_ptr_array(folder, i, folders)
-			{
-				gchar *uri = g_filename_to_uri(folder, NULL, NULL);
-				GVariant *folder_variant;
-
-				folder_variant = JSONRPC_MESSAGE_NEW(
-					"uri", JSONRPC_MESSAGE_PUT_STRING(uri),
-					"name", JSONRPC_MESSAGE_PUT_STRING(folder)
-				);
-				g_ptr_array_add(arr, folder_variant);
-
-				g_free(uri);
-			}
-
-			folders_variant = g_variant_take_ref(g_variant_new_array(G_VARIANT_TYPE_VARDICT,
-				(GVariant **)arr->pdata, arr->len));
-
-			reply_async(srv, method, client, id, folders_variant);
-
-			g_variant_unref(folders_variant);
-			g_ptr_array_free(arr, TRUE);
-		}
-		else
-			reply_async(srv, method, client, id, NULL);
-
-		g_ptr_array_free(folders, TRUE);
-
+	if (handled)
+	{
+		reply_async(srv, method, client, id, msg);
+		if (msg)
+			g_variant_unref(msg);
 		return TRUE;
 	}
 	else
